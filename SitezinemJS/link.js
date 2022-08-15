2d25d456a688d3ae61995ddf0fd6838a8d8d60ca
Pedro Ayres <pedrocgb21@gmail.com>
2022-08-14 22:19:27 -0300

    teste

--- SitezinemJS/link.js
+++
@@ -1,4 +0,0 @@
-var link01 = document.getElementsByClassName ("link01")
-console.log(link01)
-function link0101(){window.location.href = "Sitezin02.html"}
-function Voltar01(){window.location.href = "Sitezin01.html"}
